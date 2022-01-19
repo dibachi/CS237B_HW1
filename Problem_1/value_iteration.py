2cf6678e39e238f09a953ab34e8c0494e4edf5d6
dibachi <jackdibachi@gmail.com>
2022-01-18 16:20:12 -0800

    first changes to value_iteration

--- Problem_1/value_iteration.py
+++ Problem_1/value_iteration.py
@@ -15,7 +15,7 @@ def value_iteration(problem, reward, terminal_mask, gam):
     assert terminal_mask.ndim == 1 and reward.ndim == 2
 
     # perform value iteration
-    for _ in range(1000):
+    for i in range(10): #changed to i for debugging (and needs to be 1000 iters)
         ######### Your code starts here #########
         # perform the value iteration update
         # V has shape [sdim]; sdim = n * n is the total number of grid state
@@ -28,13 +28,34 @@ def value_iteration(problem, reward, terminal_mask, gam):
 
         # compute the next value function estimate for the iteration
         # compute err = tf.linalg.norm(V_new - V_prev) as a breaking condition
-
+        pxp = tf.zeros([sdim])
+        possible_values = np.zeros(adim) #np is on purpose for assignment to work
+        V_new = np.zeros(sdim) #we assign values in the loop, then convert to tensor for V update
+        V_prev = tf.cast(V, tf.float32)
+        # policy = np.zeros(sdim)
+        
+        for x in range(sdim):
+            px = np.zeros(sdim) #initial probability distribution over states
+            px[x] = 1 #we know the state is x, so px = 1 @ x
+            px = tf.convert_to_tensor(px, dtype=tf.float32)
+            for u in range(adim):
+                pxp = tf.linalg.matvec(tf.convert_to_tensor(Ts[u], dtype=tf.float32), px) #get probability distributions over all x' for each action u
+                temp0 = reward[x, u] + gam*tf.tensordot(pxp, V_prev, 1) #bellman update, dot does multiplication and sumation in place
+                if terminal_mask[x] == 1: #if terminal state, update is just the reward @ x, u
+                    temp0 = reward[x, u] #temp0 is a tensor in both cases
+                possible_values[u] = temp0 #save value for action u at current state
+            V_new[x] = np.max(possible_values) #write the optimal value at state x for optimal action
+            # policy[x] = np.argmax(possible_values)
+        V_new = tf.convert_to_tensor(V_new) #for compatibility
+        V = V_new #write updated values from temporary variable to V
+        err = tf.norm(tf.cast(V_new, tf.float32) - tf.cast(V_prev, tf.float32)) #calc error term
+        print(f"Got here {i}")
         ######### Your code ends here ###########
 
         if err < 1e-7:
             break
 
-    return V
+    return V 
 
 
 # value iteration ##############################################################
